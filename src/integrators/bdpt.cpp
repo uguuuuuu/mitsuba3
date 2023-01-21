e99db40d200395c32e015da0be6974eeefd4ca67
siwei <siwei.zhou@marquette.edu>
2023-01-21 20:18:11 +0900

    Continue implementing BDPT.

--- src/integrators/bdpt.cpp
+++ src/integrators/bdpt.cpp
@@ -3,18 +3,100 @@
 NAMESPACE_BEGIN(mitsuba)
 
 template <typename Float, typename Spectrum>
-class BDPTIntegrator : public AdjointIntegrator<Float, Spectrum> {
+class BDPTIntegrator : public MonteCarloIntegrator<Float, Spectrum> {
 
 public:
-    MI_IMPORT_BASE(AdjointIntegrator, m_max_depth, m_rr_depth, m_hide_emitters)
+    MI_IMPORT_BASE(MonteCarloIntegrator, m_hide_emitters, m_max_depth, m_rr_depth)
     MI_IMPORT_TYPES(Scene, Film, Sampler, ImageBlock, Emitter, EmitterPtr,
                     Sensor, SensorPtr, BSDF, BSDFPtr)
 
     BDPTIntegrator(const Properties &props) : Base(props) {
     }
 
-    void sample(const Scene *scene, const Sensor *sensor, Sampler *sampler,
-                ImageBlock *block, ScalarFloat sample_scale) const override {
+    void render_sample(const Scene *scene,
+                       const Sensor *sensor,
+                       Sampler *sampler,
+                       ImageBlock *block,
+                       Float *aovs,
+                       const Vector2f &pos,
+                       ScalarFloat diff_scale_factor,
+                       Mask active) const {
+        const Film *film = sensor->film();
+        const bool has_alpha = has_flag(film->flags(), FilmFlags::Alpha);
+        const bool box_filter = film->rfilter()->is_box_filter();
+
+        ScalarVector2f scale = 1.f / ScalarVector2f(film->crop_size()),
+                       offset = -ScalarVector2f(film->crop_offset()) * scale;
+
+        Vector2f sample_pos   = pos + sampler->next_2d(active),
+                 adjusted_pos = dr::fmadd(sample_pos, scale, offset);
+
+        Point2f aperture_sample(.5f);
+        if (sensor->needs_aperture_sample())
+            aperture_sample = sampler->next_2d(active);
+
+        Float time = sensor->shutter_open();
+        if (sensor->shutter_open_time() > 0.f)
+            time += sampler->next_1d(active) * sensor->shutter_open_time();
+
+        Float wavelength_sample = 0.f;
+        if constexpr (is_spectral_v<Spectrum>)
+            wavelength_sample = sampler->next_1d(active);
+
+        auto [ray, ray_weight] = sensor->sample_ray_differential(
+            time, wavelength_sample, adjusted_pos, aperture_sample);
+
+        if (ray.has_differentials)
+            ray.scale_differential(diff_scale_factor);
+
+        // Modifications for BDPT
+        ScalarVector2u film_size = film->size(),
+                       crop_size = film->crop_size();
+        uint32_t spp = sampler->sample_count();
+        ScalarFloat sample_scale =
+            dr::prod(crop_size) / ScalarFloat(spp * dr::prod(film_size));
+        // To accumulate and splat into a single block
+        block->set_normalize(true);
+        bool coalesce = block->coalesce();
+        block->set_coalesce(false);
+
+        auto [spec, valid] = sample(
+            scene, sensor, sampler, ray, block, sample_scale
+            );
+        block->set_coalesce(coalesce);
+
+        UnpolarizedSpectrum spec_u = unpolarized_spectrum(ray_weight * spec);
+
+        Color3f rgb;
+        if constexpr (is_spectral_v<Spectrum>)
+            rgb = spectrum_to_srgb(spec_u, ray.wavelengths, active);
+        else if constexpr (is_monochromatic_v<Spectrum>)
+            rgb = spec_u.x();
+        else
+            rgb = spec_u;
+
+        aovs[0] = rgb.x();
+        aovs[1] = rgb.y();
+        aovs[2] = rgb.z();
+
+        if (unlikely(has_alpha)) {
+            aovs[3] = dr::select(valid, Float(1.f), Float(0.f));
+            aovs[4] = 0.f;
+        } else {
+            aovs[3] = 0.f;
+        }
+        //
+
+        // With box filter, ignore random offset to prevent numerical instabilities
+        block->put(box_filter ? pos : sample_pos, aovs, active);
+    }
+
+    std::pair<Spectrum, Mask> sample(const Scene *scene,
+                const Sensor *sensor,
+                Sampler *sampler,
+                const RayDifferential3f &ray,
+                ImageBlock *block,
+                ScalarFloat sample_scale) const {
         auto [camera_verts, n_camera_verts] = generate_camera_subpath();
 
         auto [light_verts, n_light_verts] = generate_light_subpath();
@@ -30,12 +112,14 @@ public:
                 result += connect_bdpt();
             }
         }
-
-        // Splat
     }
 
 
-    UInt32 generate_camera_subpath() const;
+    UInt32 generate_camera_subpath() const {
+        // Generate camera ray
+
+        // Perform random walk to construct subpath
+    }
 
     UInt32 generate_light_subpath() const;
 
@@ -58,9 +142,11 @@ public:
         // Multiply radiance by MIS weight
     }
 
+    Float mis_weight() const;
+
     MI_DECLARE_CLASS()
 };
 
-MI_IMPLEMENT_CLASS_VARIANT(BDPTIntegrator, AdjointIntegrator);
+MI_IMPLEMENT_CLASS_VARIANT(BDPTIntegrator, MonteCarloIntegrator);
 MI_EXPORT_PLUGIN(BDPTIntegrator, "Bidirectional Path Tracer integrator")
 NAMESPACE_END(mitsuba)
