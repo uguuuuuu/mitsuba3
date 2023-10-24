afb2f4982f08a7ddf5571d23f6360bdfc8e420fe
siwei <736921514@qq.com>
2023-10-24 12:59:28 +0900

    No longer mark m_normalize as unlikely in ImageBlock.read()

--- src/render/imageblock.cpp
+++ src/render/imageblock.cpp
@@ -646,7 +646,7 @@ MI_VARIANT void ImageBlock<Float, Spectrum>::read(const Point2f &pos_,
         }
 
         // Normalize sample contribution if desired
-        if (unlikely(m_normalize)) {
+        if (m_normalize) {
             Float wx = 0.f, wy = 0.f;
 
             for (uint32_t i = 0; i < count.x(); ++i)
